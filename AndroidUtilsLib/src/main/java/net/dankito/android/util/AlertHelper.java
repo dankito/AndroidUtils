fb2cb6be0c662b7e4f4779602bf8c689e3c7bc38
dankito <dankito@user.noreply.github.com>
2017-02-11 20:35:40 +0100

    Removed error also from parameter names

--- AndroidUtilsLib/src/main/java/net/dankito/android/util/AlertHelper.java
+++ AndroidUtilsLib/src/main/java/net/dankito/android/util/AlertHelper.java
@@ -3,32 +3,29 @@ package net.dankito.android.util;
 import android.app.Activity;
 import android.support.v7.app.AlertDialog;
 
-/**
- * Created by ganymed on 14/11/16.
- */
 
 public class AlertHelper {
 
-  public static void showMessageThreadSafe(final Activity activity, final String errorMessage) {
-    showMessageThreadSafe(activity, errorMessage, null);
+  public static void showMessageThreadSafe(final Activity activity, final String message) {
+    showMessageThreadSafe(activity, message, null);
   }
 
-  public static void showMessageThreadSafe(final Activity activity, final String errorMessage, final String alertTitle) {
+  public static void showMessageThreadSafe(final Activity activity, final String message, final String alertTitle) {
     activity.runOnUiThread(new Runnable() {
       @Override
       public void run() {
-        showMessage(activity, errorMessage, alertTitle);
+        showMessage(activity, message, alertTitle);
       }
     });
   }
 
-  public static void showMessage(Activity activity, String errorMessage) {
-    showMessage(activity, errorMessage, null);
+  public static void showMessage(Activity activity, String message) {
+    showMessage(activity, message, null);
   }
 
-  public static void showMessage(Activity activity, String errorMessage, final String alertTitle) {
+  public static void showMessage(Activity activity, String message, final String alertTitle) {
     AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-    builder = builder.setMessage(errorMessage);
+    builder = builder.setMessage(message);
 
     if(alertTitle != null) {
       builder = builder.setTitle(alertTitle);
