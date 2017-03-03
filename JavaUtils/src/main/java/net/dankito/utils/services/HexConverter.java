22a1bd2be18b1339f0612eef8514eb918455a33f
dankito <dankito@user.noreply.github.com>
2017-03-03 22:37:03 +0100

    Implemented alternative to create byte array from hex string

--- JavaUtils/src/main/java/net/dankito/utils/services/HexConverter.java
+++ JavaUtils/src/main/java/net/dankito/utils/services/HexConverter.java
@@ -3,6 +3,9 @@ package net.dankito.utils.services;
 
 public class HexConverter {
 
+  protected static final char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+
   public byte[] hexStringToByteArray(String hexString) {
     byte[] bytes = new byte[hexString.length() / 2];
 
@@ -15,14 +18,26 @@ public class HexConverter {
     return bytes;
   }
 
-  public String byteArrayToHexString(byte[] bytes) {
+  public String byteArrayToHexStringViaStringFormat(byte[] bytes) {
     StringBuilder stringBuilder = new StringBuilder();
 
     for (byte singleByte : bytes) {
-      stringBuilder.append(String.format("%02X ", singleByte));
+      stringBuilder.append(String.format("%02X", singleByte));
     }
 
     return stringBuilder.toString();
+  }
+
+  public static String byteArrayToHexString(byte[] bytes) {
+    char[] hexChars = new char[bytes.length * 2];
+
+    for ( int j = 0; j < bytes.length; j++ ) {
+      int v = bytes[j] & 0xFF;
+      hexChars[j * 2] = hexArray[v >>> 4];
+      hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+    }
+
+    return new String(hexChars);
   }
 
 }
